d1f419ce6552c55ff0ba5f3ecd9f0ced838b94ca
Guthen <33220603+Guthen@users.noreply.github.com>
2019-10-12 13:14:06 +0200

    Update GNLib.RequireFolder
    GNLib.RequireFolder( path, should_print, indent )

--- lua/gnlib/shared/sh_recursive.lua
+++ lua/gnlib/shared/sh_recursive.lua
@@ -21,32 +21,34 @@ function GNLib.RecursiveFind( path, filter ) --GNLib.RecursiveFind("models/weapo
 	return tbl
 end
 
-function GNLib.RequireFolder( path )
+--	> Allow you to require your addon folder easily
+function GNLib.RequireFolder( path, should_print, indent )
 	local files, folders = file.Find( path .. "/*", "LUA" )
+	local indent = indent or ""
     for _, v in pairs( files ) do
         if not v:EndsWith( ".lua" ) then continue end
         if v:StartWith( "cl_" ) then
             if SERVER then
                 AddCSLuaFile( path .. "/" .. v )
-                --print( "AddCSLuaFile : " .. path .. "/" .. v )
+                if should_print then print( indent .. "AddCSLuaFile : " .. path .. "/" .. v ) end
             else
                 include( path .. "/" .. v )
-                --print( "include : " .. path .. "/" .. v )
+                if should_print then print( indent .. "include : " .. path .. "/" .. v ) end
             end
         elseif v:StartWith( "sv_" ) then
             include( path .. "/" .. v )
-            --print( "include : " .. path .. "/" .. v )
+            if should_print then print( indent .. "include : " .. path .. "/" .. v ) end
 		elseif v:StartWith( "sh_" ) then
             if SERVER then
                 AddCSLuaFile( path .. "/" .. v )
-                --print( "AddCSLuaFile : " .. path .. "/" .. v )
+                if should_print then print( indent .. "AddCSLuaFile : " .. path .. "/" .. v ) end
             end
             include( path .. "/" .. v )
-            --print( "include : " .. path .. "/" .. v )
+            if should_print then print( indent .. "include : " .. path .. "/" .. v ) end
 		end
     end
 
-    for _, f in pairs( folders ) do table.Add( folders, GNLib.RequireFolder( path .. "/" .. f ) ) end
+    for _, f in pairs( folders ) do table.Add( folders, GNLib.RequireFolder( path .. "/" .. f, should_print, indent .. "\v" ) ) end
 
     return files, folders
 end
