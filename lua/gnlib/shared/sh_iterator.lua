89ef9c383fce145a83a6acda521e50ad122feff7
Guthen <33220603+Guthen@users.noreply.github.com>
2019-11-27 08:41:43 +0100

    + Generic loop iterators: IterateChars + IterateWords

---
+++ lua/gnlib/shared/sh_iterator.lua
@@ -0,0 +1,34 @@
+
+function GNLib.IterateChars( txt )
+	local i = 0
+	return function()
+    if i >= #txt then return end
+
+		i = i + 1
+		return txt:sub( i, i )
+	end
+end
+
+function GNLib.IterateWords( txt )
+  local words = {}
+  do 
+    local word = ""
+    for c in iterateChars( txt ) do
+      if c == " " then
+        words[#words + 1] = word
+        word = ""
+      else
+        word = word .. c    
+      end
+    end
+    if #word > 0 then words[#words + 1] = word end
+  end
+
+  local i = 0
+  return function()
+    if i >= #words then return end
+
+    i = i + 1
+    return words[i]
+  end
+end
