efad80908a502c3efbdba330d472aee89b72f180
Guthen <33220603+Guthen@users.noreply.github.com>
2019-11-27 08:50:46 +0100

    correct indent to sh_iterator.lua

--- lua/gnlib/shared/sh_iterator.lua
+++ lua/gnlib/shared/sh_iterator.lua
@@ -2,7 +2,7 @@
 function GNLib.IterateChars( txt )
 	local i = 0
 	return function()
-    if i >= #txt then return end
+	if i >= #txt then return end
 
 		i = i + 1
 		return txt:sub( i, i )
@@ -10,25 +10,25 @@ function GNLib.IterateChars( txt )
 end
 
 function GNLib.IterateWords( txt )
-  local words = {}
-  do 
-    local word = ""
-    for c in iterateChars( txt ) do
-      if c == " " then
-        words[#words + 1] = word
-        word = ""
-      else
-        word = word .. c    
-      end
-    end
-    if #word > 0 then words[#words + 1] = word end
-  end
+	local words = {}
+  	do 
+    	local word = ""
+    	for c in iterateChars( txt ) do
+      		if c == " " then
+        		words[#words + 1] = word
+        		word = ""
+      		else
+        		word = word .. c    
+      		end
+    	end
+    	if #word > 0 then words[#words + 1] = word end
+  	end
 
-  local i = 0
-  return function()
-    if i >= #words then return end
+  	local i = 0
+  	return function()
+    	if i >= #words then return end
 
-    i = i + 1
-    return words[i]
-  end
+    	i = i + 1
+    	return words[i]
+  	end
 end
