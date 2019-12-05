98898660a8d0f4a658a035b7df5d9e7721753835
Nogitsu <nogitsu.9@gmail.com>
2019-12-05 21:22:43 +0100

    Updated GCA Link to the website one (https://g-ca.fr/)

--- lua/gnlib/shared/sh_util.lua
+++ lua/gnlib/shared/sh_util.lua
@@ -40,7 +40,7 @@ function GNLib.SendDiscordEmbed( webhook, title, msg, footer )
     http.Post( "https://guthen.000webhostapp.com/discord_embed.php", { url = webhook, msg = msg, title = title, footer = footer } )
 end
 
---  > This function come from GMod Creators Area (http://discord.gg/gca)
+--  > This function come from GMod Creators Area (https://g-ca.fr/)
 function GNLib.Benchmark( callback, name )
     local start = SysTime()
     
