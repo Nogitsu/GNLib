cc275ccb38275e5776d7f252409560d2d6de6733
Guthen <33220603+Guthen@users.noreply.github.com>
2020-01-25 16:44:07 +0100

    sh_iterators: fully documented

--- lua/gnlib/shared/sh_iterator.lua
+++ lua/gnlib/shared/sh_iterator.lua
@@ -1,40 +1,48 @@
 
+--- @title:
+--- 	GNLib.IterateChars: <function> Iterate throug characters of a string
+--- @note:
+--- 	You should use it in a for-loop
+--- @params:
+--- 	txt: <string> Text to iterate
+--- @return:
+--- 	closure: <function> Function used to iterate
+--- @example:
+--- 	#prompt: Iterate each character through "GNLib <3"
+--- 	#code: for c in GNLib.IterateChars( "GNLib <3" ) do\n\tprint( c )\nend
+--- 	#output: https://cdn.discordapp.com/attachments/638822462431166495/670653540485693442/unknown.png
 function GNLib.IterateChars( txt )
-	--[[ local i = 0
-	return function()
-		if i >= #txt then return end
-
-		i = i + 1
-		return txt:sub( i, i )
-	end ]]
 	return txt:gmatch( "." )
 end
 
+--- @title:
+--- 	GNLib.IterateWords: <function> Iterate throug words of a string
+--- @note:
+--- 	You should use it in a for-loop
+--- @params:
+--- 	txt: <string> Text to iterate
+--- @return:
+--- 	closure: <function> Function used to iterate
+--- @example:
+--- 	#prompt: Iterate each word through "GNLib <3"
+--- 	#code: for w in GNLib.IterateWords( "GNLib <3" ) do\n\tprint( w )\nend
+--- 	#output: https://cdn.discordapp.com/attachments/638822462431166495/670654107702394910/unknown.png
 function GNLib.IterateWords( txt )
-	--[[local words = {}
-  	do 
-    	local word = ""
-    	for c in GNLib.IterateChars( txt ) do
-      		if c == " " then
-        		words[#words + 1] = word
-        		word = ""
-      		else
-        		word = word .. c    
-      		end
-    	end
-    	if #word > 0 then words[#words + 1] = word end
-  	end
-
-  	local i = 0
-  	return function()
-    	if i >= #words then return end
-
-    	i = i + 1
-    	return words[i]
-  	end]]--
 	return txt:gmatch( "[^% ]+" ) 
 end
 
+--- @title:
+--- 	GNLib.IterateLines: <function> Iterate throug lines of a string
+--- @note:
+--- 	You should use it in a for-loop
+--- @params:
+--- 	txt: <string> Text to iterate
+--- @return:
+--- 	closure: <function> Function used to iterate
+--- @example:
+--- 	#prompt: Iterate each line through "GNLib <3"
+--- 	#code: for l in GNLib.IterateLines( "GNLib <3\nBetter than Anything\n:kappa:" ) do\n\tprint( l )\nend
+--- 	#output: https://cdn.discordapp.com/attachments/638822462431166495/670654870382182440/unknown.png
 function GNLib.IterateLines( txt )
 	return txt:gmatch( "[^\n]+" )
 end
