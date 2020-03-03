e019c20e20b171fb971f624ad6590f1acdca79a0
Guthen <33220603+Guthen@users.noreply.github.com>
2020-03-03 20:16:25 +0100

    GNLabel: fix weird position cause by auto resize

--- lua/vgui/gnlabel.lua
+++ lua/vgui/gnlabel.lua
@@ -3,6 +3,9 @@ local PANEL = {}
 AccessorFunc( PANEL, "x_align", "AlignX" )
 AccessorFunc( PANEL, "y_align", "AlignY" )
 
+AccessorFunc( PANEL, "x_offset", "OffsetX" )
+AccessorFunc( PANEL, "y_offset", "OffsetY" )
+
 function PANEL:Init()
     self.color = GNLib.Colors.Silver
 
@@ -12,13 +15,17 @@ function PANEL:Init()
     self.x_align = TEXT_ALIGN_CENTER
     self.y_align = TEXT_ALIGN_CENTER
 
+    self.x_offset = 25
+    self.y_offset = 4
+
     self:UpdateSize()
 end
 
 function PANEL:UpdateSize()
-    surface.SetFont( self.font or "GNLFontB15" )
-    local tw, th = surface.GetTextSize( self.text or "" )
-    self:SetSize( math.max( self:GetWide(), tw + 25 ), math.max( self:GetTall(), th + 4 ) )
+    surface.SetFont( self.font )
+    local tw, th = surface.GetTextSize( self.text )
+    --self:SetSize( math.max( self:GetWide(), tw + 25 ), math.max( self:GetTall(), th + 4 ) )
+    self:SetSize( tw + self.x_offset, th + self.y_offset )
 end    
 --  > Change size on font and text update
 
@@ -44,6 +51,7 @@ end
 
 --  > Override existing
 function PANEL:Paint( w, h )
+    --draw.RoundedBox( 0, 0, 0, w, h, color_white)
     draw.SimpleText( self.text, self.font, (self.x_align == TEXT_ALIGN_LEFT) and 0 or (self.x_align == TEXT_ALIGN_CENTER) and w / 2 or (self.x_align == TEXT_ALIGN_RIGHT) and w, (self.y_align == TEXT_ALIGN_TOP) and 0 or (self.y_align == TEXT_ALIGN_CENTER) and h / 2 or (self.y_align == TEXT_ALIGN_BOTTOM) and h, self.color, self.x_align, self.y_align )
 end
 
