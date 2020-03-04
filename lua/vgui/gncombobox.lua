4a9a6dd710472830d9cd8c1e83ad44f09694a035
Guthen <33220603+Guthen@users.noreply.github.com>
2020-03-04 17:55:55 +0100

    GNComboBox: GetValue + choices auto-size + menu tall fix

--- lua/vgui/gncombobox.lua
+++ lua/vgui/gncombobox.lua
@@ -10,11 +10,12 @@ AccessorFunc( PANEL, "value", "Value", FORCE_STRING )
 
 AccessorFunc( PANEL, "reseter", "Reseter", FORCE_BOOL )
 
+--  > Base functions
+
 function PANEL:Init()
-    self.self_h = 25
     self.menu_offset = 5
 
-    self:SetSize( 100, self.self_h )
+    self:SetSize( 100, 25 )
     self:SetText( "" )
     self:SetCursor( "hand" ) 
 
@@ -31,19 +32,38 @@ function PANEL:Init()
     self.text_color = GNLib.Colors.WetAsphalt
 end
 
+function PANEL:Paint( w, h )
+    GNLib.DrawElipse( 0, 0, w, h, self:IsHovered() and self.hovered_color or self.color )
+    GNLib.DrawTriangle( w - 15, h / 2, 8, self:IsHovered() and GNLib.Colors.MidnightBlue or GNLib.Colors.WetAsphalt, self:IsMenuOpen() and 2 or 0 )
+
+    draw.SimpleText( self:GetSelected() and self:GetSelected().text or self:GetValue(), self.font, 10, h / 2, self.text_color, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
+end
+
+function PANEL:IsHovered()
+    local x, y = self:LocalCursorPos()
+    return x <= self:GetWide() and y <= self:GetTall() and 0 <= x and 0 <= y
+end
+
+function PANEL:DoClick()
+    if self:IsMenuOpen() then 
+        self:CloseMenu()
+    else
+        self:OpenMenu()
+    end
+end
+
+function PANEL:OnRemove()
+    self:CloseMenu()
+end
+
+--  > Custom functions
+
 function PANEL:OnSelect( id, value, data )
     --  > Overwrite this func
 end
 
 function PANEL:OnReset()
     --  > Overwrite this func
-end
-
-function PANEL:Paint( w, h )
-    GNLib.DrawElipse( 0, 0, w, h, self:IsHovered() and self.hovered_color or self.color )
-    GNLib.DrawTriangle( w - 15, h / 2, 8, self:IsHovered() and GNLib.Colors.MidnightBlue or GNLib.Colors.WetAsphalt, self:IsMenuOpen() and 2 or 0 )
-
-    draw.SimpleText( self:GetSelected() and self:GetSelected().text or self:GetValue(), self.font, 10, h / 2, self.text_color, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
 end
 
 function PANEL:AddChoice( text, data, auto_select )
@@ -54,7 +74,7 @@ function PANEL:AddChoice( text, data, auto_select )
 end
 
 function PANEL:CalcTall()
-    self.menu_tall = math.min( self.self_h + self.menu_offset + ( #self.choices + ( self.reseter and 1 or 0 ) ) * self.self_h, self.max_menu_tall )
+    self.menu_tall = math.min( self:GetTall() + self.menu_offset + ( #self.choices + ( self.reseter and 1 or 0 ) ) * self:GetTall(), self.max_menu_tall )
 end
 
 function PANEL:SetReseter( bool )
@@ -65,9 +85,9 @@ function PANEL:SetReseter( bool )
     self:CalcTall()
 end
 
-function PANEL:IsHovered()
-    local x, y = self:LocalCursorPos()
-    return x <= self:GetWide() and y <= self.self_h and 0 <= x and 0 <= y
+function PANEL:GetValue()
+    local selected = self:GetSelected()
+    return selected and selected.text or self.value
 end
 
 function PANEL:GetSelected()
@@ -76,14 +96,6 @@ end
 
 function PANEL:SetSelected( id )
     self.selected = math.Clamp( id, 1, #self.choices )
-end
-
-function PANEL:DoClick()
-    if self:IsMenuOpen() then 
-        self:CloseMenu()
-    else
-        self:OpenMenu()
-    end
 end
 
 function PANEL:OpenMenu()
@@ -100,10 +112,21 @@ function PANEL:OpenMenu()
         end
     end
 
+    --  > Set good choice width
+    local choice_w = W
+
+    surface.SetFont( self.font )
+    for i, v in ipairs( self.choices ) do
+        choice_w = math.max( choice_w, surface.GetTextSize( v.text ) )
+    end
+
+    local button_space = 10
+    local menu_w = choice_w + button_space * 2
+    --  > Create Menu scrollpanel
     local x, y = self:GetPos()
     self.Menu = vgui.Create( "DScrollPanel" )
-        self.Menu:SetPos( x + parent_x, y + parent_y + self:GetTall() + self.menu_offset )
-        self.Menu:SetSize( W, self.menu_tall - self:GetTall() )
+        self.Menu:SetSize( menu_w, self.menu_tall - self:GetTall() )
+        self.Menu:SetPos( x + parent_x + self:GetWide() / 2 - self.Menu:GetWide() / 2, y + parent_y + self:GetTall() + self.menu_offset )
         self.Menu.Paint = function() end
         self.Menu:MakePopup()
 
@@ -111,16 +134,16 @@ function PANEL:OpenMenu()
         vbar:SetHideButtons( true )
         vbar:SetWide( 0 )
 
+    --  > Create choices
     local y = 0
-    local button_space = 10
     local function add_choice( id, is_first, is_last, text, data, reseter )
         local hovered_color = reseter and GNLib.Colors.Pomegranate or self.hovered_color
         local default_color = reseter and GNLib.Colors.Alizarin or self.color
 
         local choice = self.Menu:Add( "DButton" )
-            choice:SetPos( button_space, y * self.self_h )
-            choice:SetWide( W - button_space * 2 ) 
-            choice:SetTall( self.self_h )
+            choice:SetPos( 0, y * self:GetTall() )
+            choice:SetWide( menu_w ) 
+            choice:SetTall( self:GetTall() )
             choice:SetText( "" )
             choice.Paint = function( _self, w, h ) 
                 draw.RoundedBoxEx( 8, 0, 0, w, h, _self:IsHovered() and hovered_color or default_color, is_first, is_first, is_last, is_last )
@@ -160,10 +183,6 @@ end
 
 function PANEL:IsMenuOpen()
     return self.Menu and true or false
-end
-
-function PANEL:OnRemove()
-    self:CloseMenu()
 end
 
 vgui.Register( "GNComboBox", PANEL, "DButton" )
